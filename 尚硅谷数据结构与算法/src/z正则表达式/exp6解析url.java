4c8a2a8a0e46a2749f394b78b61929dbb51f52e2
jeff <cjfabcd1987@126.com>
2021-10-26 22:37:49 +0800

    10.26 22：37

--- 尚硅谷数据结构与算法/src/z正则表达式/exp6解析url.java
+++ 尚硅谷数据结构与算法/src/z正则表达式/exp6解析url.java
@@ -15,13 +15,18 @@ import java.util.regex.Pattern;
 
 public class exp6解析url {
     public static void main(String[] args) {
-        String regStr = "[/://.]";
+        String regStr = "^([a-zA-Z]+)://([A-Za-z.]+):(\\d+)[\\w-/]*/([\\w.]+)$";
         String content = "http://www.sohu.com:8080/abc/index.htm";
         Pattern p = Pattern.compile(regStr);
         Matcher m = p.matcher(content);
-        String[] split = content.split("/|:|\\.");
-        for (String s :split){
-            System.out.println(s);
+        if (m.matches()){
+            System.out.println("匹配成功");
+            System.out.println("协议："+m.group(1));
+            System.out.println("域名："+m.group(2));
+            System.out.println("端口："+m.group(3));
+            System.out.println("文件名："+m.group(4));
+        }else {
+            System.out.println("匹配失败");
         }
     }
 }
