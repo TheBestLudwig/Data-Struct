90f9f557d0839d96f501c583d8be848e79acfccb
jeff cao <AttackOnJeff01@outlook.com>
2021-11-08 17:43:09 +0800

    11.8 17:43

--- 尚硅谷数据结构与算法/src/c栈/CalcDemo.java
+++ 尚硅谷数据结构与算法/src/c栈/CalcDemo.java
@@ -1,92 +1,116 @@
-package c栈;
-
 /**
- * 1.得到算式
- * 2.遍历算式，取出当前字符，如果是数字，则入数字栈，是符号则分情况
- *  2.1符号栈为空，直接入栈
- *  2.2符号栈不空，如当前符号优先级大于符号栈顶符号，则数字栈弹出2个数，用当前符号运算，结果存入数字栈
- *  2.3符号栈不空，如当前符号优先级小于符号栈顶符号，则直接入符号栈
- * 3.算式遍历完后，数字栈中弹出2数，用符号栈中弹出的符号运算，结果入数字栈，直到符号栈中只有一个数字
- *
- */
-public class CalcDemo {
-    public static void main(String[] args) {
-        //1.得到算式
-//        System.out.print("请输入算式：");
-//        Scanner sc = new Scanner(System.in);
-//        String s = null;
-//        if (sc.hasNext()){
-//            s = sc.next();
-//            System.out.println(s);
-//        }
-        String calc = "78+9*6+3*2";
-        String regStr1 = "[\\+\\-\\*\\/]";
-        String regStr2 = "[0-9]+";
 
-        String[] ss1 = calc.split(regStr1);
-        String[] ss2 = calc.split(regStr2);
+ package c栈;
+ //自己做的版本
 
-        ArrayStack2 numStack = new ArrayStack2(10);
-        ArrayStack2 opeStack = new ArrayStack2(10);
-        for (int i = 0; i < ss1.length; i++) {
-            if (ss1[i].isEmpty()) break;
-            numStack.push(Integer.parseInt(ss1[i]));
-            if (ss2[i].isEmpty()) break;
-            opeStack.push(ss2[i]);
+ public class CalcDemo {
+ public static void main(String[] args) {
 
+ String calc = "3+2*6-4";
+ int index = 0;
+ char ch ;
+ char curOper;
+ int num1,num2,num3;
+ ArrayStack2 numStack = new ArrayStack2(10);
+ ArrayStack2 opeStack = new ArrayStack2(10);
 
-        }
-        while (!numStack.isEmpty()) {
-            System.out.println(numStack.pop());
-        }
-        while (!opeStack.isEmpty()) {
-            System.out.println(opeStack.pop());
-        }
-
-
-        //2.遍历算式
+ while (index < calc.length()){
+ ch = calc.charAt(index);
+ if (Character.isDigit(ch)){
+ int num = Integer.parseInt(ch+"");
+ numStack.push(num);
+ }else{
+ if(opeStack.isEmpty()){
+ opeStack.push(ch);
+ }else if(优先级(ch)>opeStack.peek()){
+ opeStack.push(ch);
+ }else if(优先级(ch) <=opeStack.peek()){
+ num1 = numStack.pop();
+ num2 = numStack.pop();
+ num3 = calc(num2,num1,opeStack.peek());
+ numStack.push(num3);
+ }
+ }
+ index++;
+ }
+ //下面的计算又是一个循环
+ num1 = numStack.pop();
+ num2 = numStack.pop();
+ ch = (char)opeStack.pop();
+ num3 = calc(num2,num1,ch);
+ numStack.push(num3);
 
 
-    }
-}
 
-class ArrayStack2 {
-    private final int maxSize;
-    private final int[] stack;
-    private int top = -1;
 
-    public ArrayStack2(int maxSize) {
-        this.maxSize = maxSize;
-        stack = new int[maxSize];
-    }
+ }
+ public static int calc(int num1,int num2,char ch){
+ int temp = 0;
+ switch (ch){
+ case '+':
+ temp = num1 + num2;
+ break;
+ case '-':
+ temp = num1 - num2;
+ case '*':
+ temp = num1 * num2;
+ case '/':
+ try{
+ temp = num1 / num2;
+ }catch (ArithmeticException e){
+ System.out.println("除数不能为0");
+ }
+ break;
+ default:
+ System.out.println("算式有错");
+ }
+ return temp;
+ }
 
-    public boolean isFull() {
-        return top == maxSize - 1;
-    }
-    public boolean isEmpty(){
-        return top == -1;
-    }
+ }
 
-    public void push(int data){
-        if (isFull())return;
-        stack[++top] = data;
-    }
+ class ArrayStack2 {
+ private final int maxSize;
+ private final int[] stack;
+ private int top = -1;
 
-    public int pop(){
-        if (isEmpty()){
-            throw new RuntimeException("栈空");
-        }
-        int value = stack[top];
-        top--;
-        return value;
-    }
+ public ArrayStack2(int maxSize) {
+ this.maxSize = maxSize;
+ stack = new int[maxSize];
+ }
 
-    public void show(){//遍历时从栈顶显示
-        if (isEmpty()){
-            System.out.println("栈空");
-        }
-        for (int i = top;i>=0;i--){
-            System.out.printf("stack[%d]=%d\n",i,stack[i]);
-        }
-    }
-}
+ public boolean isFull() {
+ return top == maxSize - 1;
+ }
+ public boolean isEmpty(){
+ return top == -1;
+ }
+
+ public void push(int data){
+ if (isFull())return;
+ stack[++top] = data;
+ }
+
+ public int pop(){
+ if (isEmpty()){
+ throw new RuntimeException("栈空");
+ }
+ int value = stack[top];
+ top--;
+ return value;
+ }
+
+ public void show(){//遍历时从栈顶显示
+ if (isEmpty()){
+ System.out.println("栈空");
+ }
+ for (int i = top;i>=0;i--){
+ System.out.printf("stack[%d]=%d\n",i,stack[i]);
+ }
+ }
+
+ public int peek(){//返回顶部数据，但不弹出
+ return stack[top];
+ }
+ }
+ **/
